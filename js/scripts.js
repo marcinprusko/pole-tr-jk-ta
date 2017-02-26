71106bf4759219dc065c913d0ca3cd946e9d4a97
marcinprusko <marcinprusko@gmail.com>
2017-02-26 22:55:39 +0100

    pierwszy komentarz

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+﻿// plik scripts.js
+
+var a = 3;
+
+var h = 8;
+
+var triangleArea = a*h/2;
+
+console.log( 'Pole trójkąta o podstawie a: ' + a + ' h: ' + h + ' wynosi: ' + triangleArea);
+
+var a = prompt('Wpisz rozmiar boku a');
+
+var h = prompt('Wpisz rozmiar wysokości h');
+
+console.log(triangleArea);
